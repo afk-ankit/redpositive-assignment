fc8eaca8d96732c85d98ebb1132707e99b1b8fbf
Ankit Sharma <ankitsharmagh093@gmail.com>
2022-10-31 20:56:34 +0530

    cleanup commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import './App.css';
-import Modal from './Modal';
-import ReactDOM from 'react-dom';
-import { useState } from 'react';
+
 
 
 function App() {
-  const [show, setShow] = useState(false)
+
   return (
     <div className="App">
-      <button onClick={() => {
-        setShow(!show)
-      }}>Click here to open Modal</button>
-      {show && ReactDOM.createPortal(<Modal setShow={setShow} />, document.getElementById('modal'))}
+
     </div>
   );
 }
