48affa148e54865fa7629edf97e959f7bab96128
Ankit Sharma <ankitsharmagh093@gmail.com>
2022-10-31 16:10:29 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  //This is an example of state and event
+  const [input, setInput] = useState("")
+  const [show, setShow] = useState(1)
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      {show ? <p>{input}</p> : <></>}
+
+      <input type="text" onChange={(e) => {
+        setInput(e.target.value)
+      }} placeholder={"Please enter a message"} />
+
+      <button onClick={() => {
+        setShow(!show)
+      }}>Show/Hide</button>
     </div>
   );
 }
