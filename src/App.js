f230ada0a66807ee1994666782d8b72989909618
Ankit Sharma <ankitsharmagh093@gmail.com>
2022-10-31 22:39:59 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 
 
 function App() {
+  const [load, setLoad] = useState(true)
+  const [jokes, setJokes] = useState('')
+  useEffect(() => {
+    const getData = async () => {
+      const data = await fetch('https://official-joke-api.appspot.com/random_joke');
+      const result = await data.json();
+      setJokes({
+        setup: result.setup,
+        punchline: result.punchline
+      })
+    }
+
+    getData()
+
+  }, [load])
+
 
   return (
     <div className="App">
+      <h1>Dad jokes</h1>
+      <h3>{jokes.setup}</h3>
+      <h3>{jokes.punchline}</h3>
 
+      <button onClick={() => {
+        setLoad(!load)
+      }}>Get new Jokes</button>
     </div>
   );
 }
